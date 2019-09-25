47a77ba69d3821d259ae2626a5fbaee619405806
tathagat07 <tathagat.kumar3@gmail.com>
2019-09-25 12:16:29 +0530

    second commit

--- src/main/java/com/stackroute/movie.java
+++ src/main/java/com/stackroute/movie.java
@@ -1,10 +1,20 @@
 package com.stackroute;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class movie {
-    @Autowired
+
     actor actor;
+    @Autowired
+    public movie(com.stackroute.actor actor) {
+        this.actor = actor;
+    }
+
+    public movie() {
+
+    }
 
     public com.stackroute.actor getActor() {
         return actor;
